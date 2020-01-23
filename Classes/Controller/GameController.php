f562ceb7cca7538585e139f9ef93706386b72702
Chris <pschorr.christian@gmail.com>
2020-01-23 23:23:40 +0100

    0.0.1@2020-01-23

--- Classes/Controller/GameController.php
+++ Classes/Controller/GameController.php
@@ -2,11 +2,22 @@
 
 namespace Ps\Ai\Controller;
 
+use Ps\Ai\Domain\Model\Map;
+use Ps\Ai\Domain\Repository\MapRepository;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
 class GameController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
 	/**
 	 * @return void
 	 */
 	public function indexAction() {
+
+		/** @var Map $map */
+		$map = $this->objectManager->get(MapRepository::class)->findByUid(1);
+
+		DebuggerUtility::var_dump($map);
+
+		return true;
 	}
 }
