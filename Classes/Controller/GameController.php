91ecd64561841bd52ce8f38a69e23d91959d78e8
Chris <pschorr.christian@gmail.com>
2020-01-23 23:12:59 +0100

    0.0.1@2020-01-23

---
+++ Classes/Controller/GameController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ps\Ai\Controller;
+
+class GameController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+}
